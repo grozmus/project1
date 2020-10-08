8275892e9b6dabf3466e170dbb0f5fec49fca8bb
Grzegorz Rozmus <grozmus@gmail.com>
2020-10-08 16:47:19 +0200

    my changes

---
+++ src/main/webapp/app/entities/product/product-update.tsx
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Link, RouteComponentProps } from 'react-router-dom';
+import { Button, Row, Col, Label } from 'reactstrap';
+import { AvFeedback, AvForm, AvGroup, AvInput, AvField } from 'availity-reactstrap-validation';
+import { ICrudGetAction, ICrudGetAllAction, ICrudPutAction } from 'react-jhipster';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IRootState } from 'app/shared/reducers';
+
+import { getEntity, updateEntity, createEntity, reset } from './product.reducer';
+import { IProduct } from 'app/shared/model/product.model';
+import { convertDateTimeFromServer, convertDateTimeToServer, displayDefaultDateTime } from 'app/shared/util/date-utils';
+import { mapIdList } from 'app/shared/util/entity-utils';
+
+export interface IProductUpdateProps extends StateProps, DispatchProps, RouteComponentProps<{ id: string }> {}
+
+export const ProductUpdate = (props: IProductUpdateProps) => {
+  const [isNew, setIsNew] = useState(!props.match.params || !props.match.params.id);
+
+  const { productEntity, loading, updating } = props;
+
+  const handleClose = () => {
+    props.history.push('/product');
+  };
+
+  useEffect(() => {
+    if (isNew) {
+      props.reset();
+    } else {
+      props.getEntity(props.match.params.id);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (props.updateSuccess) {
+      handleClose();
+    }
+  }, [props.updateSuccess]);
+
+  const saveEntity = (event, errors, values) => {
+    if (errors.length === 0) {
+      const entity = {
+        ...productEntity,
+        ...values,
+      };
+
+      if (isNew) {
+        props.createEntity(entity);
+      } else {
+        props.updateEntity(entity);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <Row className="justify-content-center">
+        <Col md="8">
+          <h2 id="project1App.product.home.createOrEditLabel">Create or edit a Product</h2>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col md="8">
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <AvForm model={isNew ? {} : productEntity} onSubmit={saveEntity}>
+              {!isNew ? (
+                <AvGroup>
+                  <Label for="product-id">ID</Label>
+                  <AvInput id="product-id" type="text" className="form-control" name="id" required readOnly />
+                </AvGroup>
+              ) : null}
+              <AvGroup>
+                <Label id="nameLabel" for="product-name">
+                  Name
+                </Label>
+                <AvField id="product-name" type="text" name="name" />
+              </AvGroup>
+              <AvGroup>
+                <Label id="descriptionLabel" for="product-description">
+                  Description
+                </Label>
+                <AvField id="product-description" type="text" name="description" />
+              </AvGroup>
+              <AvGroup>
+                <Label id="priceLabel" for="product-price">
+                  Price
+                </Label>
+                <AvField id="product-price" type="text" name="price" />
+              </AvGroup>
+              <Button tag={Link} id="cancel-save" to="/product" replace color="info">
+                <FontAwesomeIcon icon="arrow-left" />
+                &nbsp;
+                <span className="d-none d-md-inline">Back</span>
+              </Button>
+              &nbsp;
+              <Button color="primary" id="save-entity" type="submit" disabled={updating}>
+                <FontAwesomeIcon icon="save" />
+                &nbsp; Save
+              </Button>
+            </AvForm>
+          )}
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+const mapStateToProps = (storeState: IRootState) => ({
+  productEntity: storeState.product.entity,
+  loading: storeState.product.loading,
+  updating: storeState.product.updating,
+  updateSuccess: storeState.product.updateSuccess,
+});
+
+const mapDispatchToProps = {
+  getEntity,
+  updateEntity,
+  createEntity,
+  reset,
+};
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductUpdate);
